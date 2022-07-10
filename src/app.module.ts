6308a49f5dbb7557b6993d6a3662d4b9a785f0a5
张康 <zhangkang@zhangkangdeMacBook-Pro.local>
2022-07-10 21:54:01 +0800

    chore: 环境变量配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
+import { getConfig } from './utils/index';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      isGlobal: true,
+      load: [getConfig],
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,17 +12,26 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { BusinessException } from 'src/common/exceptions/business.exception';
+import { ConfigService } from '@nestjs/config';
 
 @Controller({
   path: 'user',
   version: '1',
 })
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  @Get('getTestName')
+  getTestName() {
+    return this.configService.get('TEST_VALUE').name;
   }
 
   @Get()
