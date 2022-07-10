a04df0ab95c8334f679d6f5426abf8c414fb10a8
张康 <zhangkang@zhangkangdeMacBook-Pro.local>
2022-07-10 22:05:08 +0800

    chore: 配置swagger文档

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 import { TransformInterceptor } from './common/interceptor/transform.interceptor';
+import { generateDocument } from './doc';
 
 declare const module: any;
 
@@ -28,6 +29,9 @@ async function bootstrap() {
 
   // 异常过滤器
   app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+
+  // 配置swagger文档
+  generateDocument(app);
 
   await app.listen(3000);
 
