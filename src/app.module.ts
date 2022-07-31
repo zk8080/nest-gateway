626e854093affa776942b478af2d6cf04d1822c0
zk8080 <zhangkang8080@163.com>
2022-07-31 20:38:00 +0800

    feat: redis配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     CacheModule.register({
       isGlobal: true,
+      store: redisStore,
+      host: getConfig('REDIS_CONFIG').host,
+      port: getConfig('REDIS_CONFIG').port,
+      auth_pass: getConfig('REDIS_CONFIG').auth,
+      db: getConfig('REDIS_CONFIG').db,
     }),
     ConfigModule.forRoot({
       ignoreEnvFile: true,
